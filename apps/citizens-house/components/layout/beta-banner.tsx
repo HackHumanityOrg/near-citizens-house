ccefbe96e37b6366a88ba78232bb1be1746dadee
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2026-01-09 15:36:48 +0200

    Update beta banner to indicate mainnet

--- apps/citizens-house/components/layout/beta-banner.tsx
+++ apps/citizens-house/components/layout/beta-banner.tsx
@@ -1,7 +1,7 @@
 export function BetaBanner() {
   return (
     <div className="fixed top-0 left-0 right-0 z-[100] w-full text-white py-1.5 flex items-center justify-center bg-[#8b5cf6]">
-      <span className="text-sm font-bold tracking-wide">Beta testing on Testnet</span>
+      <span className="text-sm font-bold tracking-wide">Beta testing on Mainnet</span>
     </div>
   )
 }
